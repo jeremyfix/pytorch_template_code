ef36da6a312accaca9404ff32b6c2c6f6b815af3
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-03-09 21:21:52 +0100

    util script with train function and checkpoint

---
+++ torchtmpl/utils.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+
+# Standard imports
+import os
+
+# External imports
+import torch
+import torch.nn
+
+
+def generate_unique_logpath(logdir, raw_run_name):
+    """
+    Generate a unique directory name
+    Argument:
+        logdir: the prefix directory
+        raw_run_name(str): the base name
+    Returns:
+        log_path: a non-existent path like logdir/raw_run_name_xxxx
+                  where xxxx is an int
+    """
+    i = 0
+    while True:
+        run_name = raw_run_name + "_" + str(i)
+        log_path = os.path.join(logdir, run_name)
+        if not os.path.isdir(log_path):
+            return log_path
+        i = i + 1
+
+
+class ModelCheckpoint(object):
+    """
+    Early stopping callback
+    """
+
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        savepath,
+        min_is_best: bool = True,
+    ) -> None:
+        self.model = model
+        self.savepath = savepath
+        self.best_score = None
+        if min_is_best:
+            self.is_better = self.lower_is_better
+        else:
+            self.is_better = self.higher_is_better
+
+    def lower_is_better(self, score):
+        return self.best_score is None or score < self.best_score
+
+    def higher_is_better(self, score):
+        return self.best_score is None or score > self.best_score
+
+    def update(self, score):
+        if self.is_better(score):
+            torch.save(self.model.state_dict(), self.savepath)
+            self.best_score = score
+            return True
+        return False
+
+
+def train(
+    model,
+    loader,
+    f_loss,
+    optimizer,
+    device,
+):
+    """
+    Train a model for one epoch, iterating over the loader
+    using the f_loss to compute the loss and the optimizer
+    to update the parameters of the model.
+    Arguments :
+    model     -- A torch.nn.Module object
+    loader    -- A torch.utils.data.DataLoader
+    f_loss    -- The loss function, i.e. a loss Module
+    optimizer -- A torch.optim.Optimzer object
+    device    -- A torch.device
+    Returns :
+    """
+
+    # We enter train mode. This is useless for the linear model
+    # but is important for layers such as dropout, batchnorm, ...
+    model.train()
+    # Get the total number of minibatches, i.e. of sub epochs
+
+    for i, (inputs, targets) in enumerate(loader):
+
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # Compute the forward propagation
+        outputs = model(inputs)
+
+        loss = f_loss(outputs, targets)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+
+        optimizer.step()
