ba67a73ac84daad06a2d105f1f6f8a62c2520381
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-03-09 19:01:31 +0100

    data module

---
+++ torchtmpl/data.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+# Standard imports
+import logging
+import random
+
+# External imports
+import torch
+import torch.utils.data
+import torchvision
+from torchvision import transforms
+
+
+def get_dataloaders(data_config, use_cuda):
+    valid_ratio = data_config["valid_ratio"]
+    batch_size = data_config["batch_size"]
+    num_workers = data_config["num_workers"]
+
+    logging.info("  - Dataset creation")
+    base_dataset = torchvision.datasets.Caltech101(
+        root=data_config["trainpath"],
+        target_type="category",
+        download=True,
+        transform=transforms.ToTensor(),
+    )
+    logging.info(f"  - I loaded {len(base_dataset)} samples")
+
+    indices = list(range(len(base_dataset)))
+    random.shuffle(indices)
+    num_valid = int(valid_ratio * len(base_dataset))
+    train_indices = indices[:num_valid]
+    valid_indices = indices[num_valid:]
+
+    train_dataset = torch.utils.data.Subset(base_dataset, train_indices)
+    valid_dataset = torch.utils.data.Subset(base_dataset, valid_indices)
+
+    # Build the dataloaders
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=use_cuda,
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=use_cuda,
+    )
+
+    num_classes = len(base_dataset.categories)
+    input_size = list(base_dataset[0][0].shape)
+
+    return train_loader, valid_loader, input_size, num_classes
