fc00a5856810988e33a1c372bd97a85dbc0e71bb
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-03-09 21:20:56 +0100

    resize to fit square size and grayscale

--- torchtmpl/data.py
+++ torchtmpl/data.py
@@ -6,9 +6,19 @@ import random
 
 # External imports
 import torch
+import torch.nn as nn
 import torch.utils.data
 import torchvision
 from torchvision import transforms
+
+import matplotlib.pyplot as plt
+
+
+def show_image(X):
+    num_c = X.shape[0]
+    plt.figure()
+    plt.imshow(X[0] if num_c == 1 else X.permute(1, 2, 0))
+    plt.show()
 
 
 def get_dataloaders(data_config, use_cuda):
@@ -17,12 +27,17 @@ def get_dataloaders(data_config, use_cuda):
     num_workers = data_config["num_workers"]
 
     logging.info("  - Dataset creation")
+
+    input_transform = transforms.Compose(
+        [transforms.Grayscale(), transforms.Resize((128, 128)), transforms.ToTensor()]
+    )
+
     base_dataset = torchvision.datasets.Caltech101(
         root=data_config["trainpath"],
-        target_type="category",
         download=True,
-        transform=transforms.ToTensor(),
+        transform=input_transform,
     )
+
     logging.info(f"  - I loaded {len(base_dataset)} samples")
 
     indices = list(range(len(base_dataset)))
