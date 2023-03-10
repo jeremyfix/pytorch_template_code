f2710507ca8ca53b54e0a3e046a7a7847edb5c16
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-03-10 19:41:15 +0100

    also skip the configs directory

--- submit-slurm.py
+++ submit-slurm.py
@@ -27,7 +27,7 @@ echo "Running on " $(hostname)
 echo "Copying the source directory and data"
 date
 mkdir $TMPDIR/code
-rsync -r --exclude logs --exclude logslurms . $TMPDIR/code
+rsync -r --exclude logs --exclude logslurms --exclude configs . $TMPDIR/code
 
 echo "Checking out the correct version of the code commit_id {commit_id}"
 cd $TMPDIR/code
